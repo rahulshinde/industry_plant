dc8dbf4287e299f91aaa715ac2db795be8eddd53
Rahul S. Shinde <rshinde@mica.edu>
2019-08-05 19:00:58 -0400

    new site

--- js/main.js
+++ js/main.js
@@ -1,37 +1,72 @@
+var current;
+
 $(document).ready(function(){
   // $(document).on('click', nextSlide);
-  $('#about_trigger').on('click', toggleAbout);
-  $('#index_trigger').on('click', toggleIndex);
   body = $('body');
+  $('.zoom_image').on('click', openSlideshow);
+
+  $('.next').on('click', nextSlide);
+  $('.prev').on('click', prevSlide);
+  $('.close').on('click', closeSlideshow);
 });
 
-nextSlide = function(){
-  $('.slide').toggleClass('current');
-  $('.slide').toggleClass('out');
+function closeSlideshow(){
+  body.removeClass('slideshow_open');
 }
 
-toggleAbout = function(){
-  if (body.hasClass('open_bottom') && !body.hasClass('show_about')){
-    body.addClass('show_about');
-    body.removeClass('show_index');
-  } else if(!body.hasClass('open_bottom')){
-    body.addClass('open_bottom');
-    body.addClass('show_about');  
-  } else{
-    body.removeClass('open_bottom');
-    body.removeClass('show_about');  
+function openSlideshow(){
+  var src = $(this).attr('src');
+  var caption = $(this).data('caption');
+  current = $(this).data('position');
+  $('.current_slide').attr('src', src);
+  document.getElementById('slideshow_caption').innerHTML = caption;
+
+  body.addClass('slideshow_open');
+}
+
+function nextSlide(){
+  var current_image = $('.image' + current);
+  current = current + 1;
+  var next_image = $('.image' + current);
+  if (current_image.data('project') == next_image.data('project')){
+    fadeTransition(next_image);
+  }else{
+    slideTransition(next_image, 'next');
   }
 }
 
-toggleIndex = function(){
-  if (body.hasClass('open_bottom') && !body.hasClass('show_index')){
-    body.addClass('show_index');
-    body.removeClass('show_about');
-  } else if(!body.hasClass('open_bottom')){
-    body.addClass('open_bottom');
-    body.addClass('show_index');  
-  } else{
-    body.removeClass('open_bottom');
-    body.removeClass('show_index');  
+function prevSlide(){
+  var current_image = $('.image' + current);
+  current = current - 1;
+  var next_image = $('.image' + current);
+  if (current_image.data('project') == next_image.data('project')){
+    fadeTransition(next_image);
+  }else{
+    slideTransition(next_image, 'prev');
   }
+}
+
+function fadeTransition(next){
+  body.addClass('fade_transition');
+  setTimeout(function(){
+    $('.current_slide').attr('src', next.attr('src'));
+    body.removeClass('fade_transition');
+  }, 450);
+}
+
+function slideTransition(next, direction){
+  next_slide = $('.next_slide');
+  body.addClass('slide_transition');
+  body.addClass(direction);
+  next_slide.attr('src', next.attr('src'));
+  setTimeout(function(){
+    body.addClass('transitioning');
+  }, 100);
+  setTimeout(function(){
+    document.getElementById('slideshow_caption').innerHTML = next.data('caption');
+    body.removeClass('transitioning');
+    body.removeClass(direction);
+    $('.current_slide').removeClass('current_slide').addClass('next_slide').attr('src', '');
+    next_slide.removeClass('next_slide').addClass('current_slide');
+  }, 700);
 }
